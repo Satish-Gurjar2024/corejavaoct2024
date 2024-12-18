dcf13169693c6b9a534d64fe1a1cfaa95149d3f3
unknown <satishgurjar2020@gmail.com>
2024-12-18 21:34:18 +0530

    initial commit

---
+++ AdditionOfTwoNumber.java
@@ -0,0 +1,23 @@
+package com.sg.operators;
+
+public class AdditionOfTwoNumber {
+	
+	public static void main(String[] args) {
+		
+		//initialization the value of variables
+		int a=150;
+		int b=233;
+		int Temp=0;
+		
+		
+		//displying show a & b
+		System.out.println("First variable a = "+a);
+		System.out.println("Second variable b = "+b);
+		
+		//addition method appling
+		Temp=a+b;
+		System.out.println("Addition Of Two Numbers Value = "+Temp);
+		
+	}
+
+}
